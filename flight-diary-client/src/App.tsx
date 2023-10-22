0689499e7d9175636f69a1dd7104120e1c34575b
pablo-maff <pablo.maff.076@gmail.com>
2023-10-22 14:23:35 +0200

    9.17

--- flight-diary-client/src/App.tsx
+++ flight-diary-client/src/App.tsx
@@ -6,6 +6,12 @@ const baseURL = 'http://localhost:3000'
 
 function App() {
   const [diaryEntries, setDiaryEntries] = useState<DiaryEntry[]>([])
+  const [newEntry, setNewEntry] = useState({
+    date: '',
+    visibility: '',
+    weather: '',
+    comment: '',
+  })
 
   useEffect(() => {
     axios.get<DiaryEntry[]>(`${baseURL}/api/diaries`).then((response) => {
@@ -13,8 +19,77 @@ function App() {
     })
   }, [])
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function handleNewEntryInputs(event: { target: { value: any; name: any } }) {
+    const { value, name } = event.target
+
+    setNewEntry((prevEntry) => ({ ...prevEntry, [name]: value }))
+  }
+
+  function handleAddEntry(event: React.SyntheticEvent) {
+    event.preventDefault()
+
+    axios
+      .post<DiaryEntry>(`${baseURL}/api/diaries`, newEntry)
+      .then((response) => {
+        setDiaryEntries((prevEntries) => prevEntries.concat(response.data))
+      })
+  }
+
   return (
     <main>
+      <section
+        id="add-diary-entry"
+        aria-label="new flight diary entry"
+      >
+        <h2>Add New Entry</h2>
+        <form onSubmit={handleAddEntry}>
+          <div>
+            <label htmlFor="new-diary-entry-date">Date</label>
+            <input
+              type="text"
+              id="new-diary-entry-date"
+              name="date"
+              onChange={handleNewEntryInputs}
+            />
+          </div>
+          <div>
+            <label htmlFor="new-diary-entry-visibility">Visibility</label>
+            <input
+              type="text"
+              id="new-diary-entry-visibility"
+              name="visibility"
+              onChange={handleNewEntryInputs}
+            />
+          </div>
+          <div>
+            <label htmlFor="new-diary-entry-weather">Weather</label>
+            <input
+              type="text"
+              id="new-diary-entry-weather"
+              name="weather"
+              onChange={handleNewEntryInputs}
+            />
+          </div>
+          <div>
+            <label htmlFor="new-diary-entry-comment">Comment</label>
+            <input
+              type="text"
+              id="new-diary-entry-comment"
+              name="comment"
+              onChange={handleNewEntryInputs}
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              id="new-diary-entry-submit"
+            >
+              Add
+            </button>
+          </div>
+        </form>
+      </section>
       <section
         id="flight-diary-entries"
         aria-label="flight diary entries"
@@ -23,7 +98,7 @@ function App() {
           id="section-title"
           aria-label="diary entries"
         >
-          Diary entries
+          Diary Entries
         </h2>
         <div id="flight-diary-entries-container">
           <ul id="flight-diary-entries-list">

7da9bed97f899568e1004b8043299c38d904bbcb
pablo-maff <pablo.maff.076@gmail.com>
2023-10-24 15:33:36 +0200

    9.23

--- patientor-client/src/components/PatientPage/index.tsx
+++ patientor-client/src/components/PatientPage/index.tsx
@@ -1,9 +1,10 @@
 import { useParams } from "react-router-dom";
 import { Patient } from "../../types";
-import { Box, Typography } from "@mui/material";
+import { Box, List, ListItem, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import patientService from "../../services/patients";
 import PatientGender from "./PatientGender";
+import { Entry } from "../../types";
 
 function assertIsString(value: unknown): asserts value is string {
   if (typeof value !== "string") {
@@ -36,6 +37,8 @@ const PatientPage = () => {
     return <p style={{ color: "red" }}>The selected patient was not found</p>;
   }
 
+  console.log("patient", patient);
+
   return (
     <Box sx={{ mt: 4 }}>
       <Box sx={{ display: "flex" }}>
@@ -46,6 +49,28 @@ const PatientPage = () => {
         <Typography>Occupation: {patient.occupation}</Typography>
         <Typography>Birthday: {patient.dateOfBirth}</Typography>
         <Typography>SSN: {patient.ssn}</Typography>
+      </Box>
+      <Box sx={{ mt: 2 }}>
+        <Typography variant="h6">Entries</Typography>
+        {patient.entries.map((entry: Entry) => (
+          <Box key={entry.id}>
+            <Box>
+              <Typography sx={{ display: "inline-block" }}>
+                {entry.date}
+              </Typography>
+              <Typography
+                sx={{ fontStyle: "italic", display: "inline-block", ml: 2 }}
+              >
+                {entry.description}
+              </Typography>
+            </Box>
+            <List sx={{ listStyleType: "disc", pl: 4 }}>
+              {entry.diagnosisCodes?.map((code) => (
+                <ListItem sx={{ display: "list-item" }}>{code}</ListItem>
+              ))}
+            </List>
+          </Box>
+        ))}
       </Box>
     </Box>
   );
--- patientor-client/src/types.ts
+++ patientor-client/src/types.ts
@@ -10,6 +10,64 @@ export enum Gender {
   Other = "other",
 }
 
+export interface Diagnose {
+  code: string;
+  name: string;
+  latin?: string;
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose["code"]>;
+}
+
+enum HealthCheckRating {
+  Healthy = 0,
+  LowRisk = 1,
+  HighRisk = 2,
+  CriticalRisk = 3,
+}
+
+export enum EntryType {
+  HealthCheck = "HealthCheck",
+  OccupationalHealthcare = "OccupationalHealthcare",
+  Hospital = "Hospital",
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: EntryType.HealthCheck;
+  healthCheckRating: HealthCheckRating;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: EntryType.OccupationalHealthcare;
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: EntryType.Hospital;
+  discharge: Discharge;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
 export interface Patient {
   id: string;
   name: string;
@@ -17,6 +75,7 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[];
 }
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
